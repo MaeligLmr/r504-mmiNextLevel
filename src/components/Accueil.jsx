a998d2b88c67df8f07c2d0758a7a6b0f9c35e798
Sharachel <kimyen.pham01@gmail.com>
2024-12-19 17:42:41 +0100

    Ajout Tailwind des créa

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -25,12 +25,17 @@ function Accueil() {
 
     return (
         <>
-            <header>
-                <h1>MMI NEXT LEVEL 😊</h1>
+            <header className="flex items-center p-8 sticky top-0 z-100">
+            <div class="logo text-xl font-bold flex-1 max-w-16 bg-[#5E3472] p-3 rounded mr-5"><img src="/img/logo_menu.png" alt="logo MMINextLevel"/></div>
+                <button class="relative font-extrabold text-2xl bg-white p-4 text-[#5E3472] rounded-full shadow-[-6px_6px_0_0_#5E3472]">
+                    ?
+                </button>
             </header>
             <main>
-                <div className="h-screen w-screen">
-                    <button onClick={scrollDown} aria-label="scroll down" className=" absolute bottom-2 left-1/2 rounded-full bg-transparent border border-white"></button>
+                <div className="h-screen w-full bg-cover bg-center">
+                    <button onClick={scrollDown} aria-label="scroll down" className=" absolute bottom-2 left-1/2 bg-transparent border">
+                        <img width="100" height="100" src="/img/fleche_bas.svg" alt="flèche vers le bas"/>
+                    </button>
                 </div>
                 {error && (
                     <Alert severity="error" sx={{ mb: 2, mx: 'auto', width: '80%' }}>
